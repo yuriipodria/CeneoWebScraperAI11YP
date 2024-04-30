279e34c9527df5dc84dc3a6430c00600e6123876
yuriipodria <urabelivr@gmail.com>
2024-04-30 18:25:26 +0200

    30/04/2024 yp

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/extract')
+def extract():
+    return render_template("extract.html")
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+@app.route('/author')
+def author():
+    return render_template("author.html")
+
+@app.route('/product/<product_id>')
+def product(product_id):
+    return render_template("product.html", product_id=product_id)
